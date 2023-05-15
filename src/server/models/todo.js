92670a2c3ae15e855574f9b33f24da17a72a4c82
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-05-15 20:34:05 +0200

    fixed formatting

--- src/server/models/todo.js
+++ src/server/models/todo.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
-    description: {type: String, requires:true, maxLength: 160},
-    deadline: {type:String, requires:true},
-    progress: {type:Number, require:true, min:0, max:100, default:0}
-})
+  description: { type: String, requires: true, maxLength: 160 },
+  deadline: { type: String, requires: true },
+  progress: { type: Number, require: true, min: 0, max: 100, default: 0 },
+});
 
 //maybe add virtual for url?
 
