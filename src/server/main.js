ca0aac732257d7b50ce9c7d065461c6069b2aed5
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-05-15 20:35:23 +0200

    all api routes are now accessed via the /api prefix

--- src/server/main.js
+++ src/server/main.js
@@ -1,37 +1,38 @@
-//impport dependencies
+// import dependencies
 const express = require("express");
 const ViteExpress = require("vite-express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-//import routers:
+// import routers:
 const Router = require("./routes.js");
 
-//init app
+// init app
 const app = express();
 
-//make sure to get json objects from post requests data
+// config
+const WEB_SERVER_PORT = 3000;
+const MONGODB_DOMAIN = "127.0.0.1";
+const MONGODB_SERVER_PORT = 27017;
+
+// make sure to get json objects from post requests data
 app.use(express.json());
 app.use(bodyParser.json());
 
+// using the imports in the application
+// when url is localhost:3000/, the router is called
+app.use("/api", Router);
 
-//using the imports in the application
-//when url is localhost:3000/ , the router is called
-app.use("/", Router);
-
-//debuging route-----------------------------
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
-//-----------------------------------------
-
-//connect to local mongo DB. (needs to be installed and running)
-mongoose.set("strictQuery", false);
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017");
+// connect to local mongo DB function.
+async function connectdb() {
+  await mongoose.connect(`mongodb://${MONGODB_DOMAIN}:${MONGODB_SERVER_PORT}`);
 }
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
+// connect to mongodb (needs to be installed and running)
+mongoose.set("strictQuery", false);
+connectdb().catch((err) => console.log(err));
+
+// start web-server
+ViteExpress.listen(app, WEB_SERVER_PORT, () =>
+  console.log(`Server is listening on port ${WEB_SERVER_PORT}...`)
 );
