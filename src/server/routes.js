4bc26c403b82e895484b084cd6c4b66eb3a10300
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-05-16 20:58:55 +0200

    backend not corresponding with frontend

--- src/server/routes.js
+++ src/server/routes.js
@@ -4,65 +4,62 @@ const asyncHandler = require("express-async-handler");
 const router = express.Router();
 const TodoModel = require("./models/todo");
 
-
-router.get("/Todos", asyncHandler (async function (req, res) {
+router.get(
+  "/Todos",
+  asyncHandler(async function (req, res) {
     const allTodos = await TodoModel.find({}).exec();
 
-    if(!Array.isArray(allTodos) || !allTodos.length){
-        throw new Error('There are no Todos');
+    if (!Array.isArray(allTodos) || !allTodos.length) {
+      throw new Error("There are no Todos");
+    } else {
+      res.send(allTodos);
     }
-    else{
-        res.send(allTodos);
-    }
-
-    })
+  })
 );
 
-router.post("/Todo/create", asyncHandler (async function (req, res) {
-
+router.post(
+  "/Todo/create",
+  asyncHandler(async function (req, res) {
     const newtodo = new TodoModel({
-        description: req.body.description,
-        deadline: req.body.deadline,
-        progress: req.body.progress,
+      description: req.body.description,
+      deadline: req.body.deadline,
+      progress: req.body.progress,
     });
 
     await newtodo.save();
     res.send(newtodo);
-
-    })
+  })
 );
 
-router.put("/Todo/edit", asyncHandler( async function (req, res) {
-
+router.put(
+  "/Todo/edit",
+  asyncHandler(async function (req, res) {
     const edittodo = await TodoModel.findById(req.body.id).exec();
 
     if (!edittodo) {
-        throw new Error("cannot edit Todo. Todo not found");
-    }
-    else{
-        edittodo.description = req.body.description;
-        edittodo.deadline = req.body.deadline;
-        edittodo.progress = req.body.progress;
-        await edittodo.save();
+      throw new Error("cannot edit Todo. Todo not found");
+    } else {
+      edittodo.description = req.body.description;
+      edittodo.deadline = req.body.deadline;
+      edittodo.progress = req.body.progress;
+      await edittodo.save();
 
-        res.send(edittodo);
+      res.send(edittodo);
     }
-
-    })
+  })
 );
 
-router.delete("/Todo/delete", asyncHandler (async function (req, res) {
+router.delete(
+  "/Todo/delete",
+  asyncHandler(async function (req, res) {
+    const deleted = await TodoModel.findByIdAndDelete(req.body.id).exec();
 
-    const deleted = await TodoModel.findByIdAndDelete(req.body._id).exec();
-    
     if (!deleted) {
-        throw new Error('cannot delete Todo. Todo not found');
+      throw new Error("cannot delete Todo. Todo not found");
+    } else {
+      res.sendStatus(200);
     }
-    else{
-        res.sendStatus(200);
-    }
-
-    })
+  })
 );
 
 module.exports = router;
