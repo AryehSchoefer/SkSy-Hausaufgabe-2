0a2788290f52f055787a40ee01a14231347b061c
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-05-16 18:26:55 +0200

    Added addTodo function responsible for posting new todo to backend

--- src/client/data/todos.js
+++ src/client/data/todos.js
@@ -3,3 +3,15 @@ export async function getTodos() {
   const jsonTodos = await todos.json();
   return jsonTodos;
 }
+
+export async function addTodo({ description, progress, deadline }) {
+  const newTodo = await fetch("http://localhost:3000/api/todo/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ description, progress, deadline }),
+  });
+  const jsonNewTodo = await newTodo.json();
+  return jsonNewTodo;
+}
