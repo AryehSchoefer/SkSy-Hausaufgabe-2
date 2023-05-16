239c4ae6e5f6429da11eaa96cd13f7e29bc97a1a
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-05-16 18:25:47 +0200

    Made NavBar mark the correct current page

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -1,6 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+  const { pathname } = useLocation();
+  const activeLinkStyle =
+    "block py-2 pl-3 pr-4 rounded text-white bg-blue-700 md:bg-transparent md:text-blue-500 md:p-0";
+  const nonActiveLinkStyle =
+    "block py-2 pl-3 pr-4 rounded text-white md:border-0 md:p-0 md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent";
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -15,8 +21,9 @@ function Navbar() {
             <li>
               <Link
                 to={""}
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
+                className={
+                  pathname === "/" ? activeLinkStyle : nonActiveLinkStyle
+                }
               >
                 Home
               </Link>
@@ -24,7 +31,9 @@ function Navbar() {
             <li>
               <Link
                 to={"add"}
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={
+                  pathname === "/add" ? activeLinkStyle : nonActiveLinkStyle
+                }
               >
                 Add TODO
               </Link>
@@ -32,7 +41,11 @@ function Navbar() {
             <li>
               <Link
                 to={"impressum"}
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={
+                  pathname === "/impressum"
+                    ? activeLinkStyle
+                    : nonActiveLinkStyle
+                }
               >
                 Impressum
               </Link>
